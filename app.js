d750a8e2ab996d643177e1b5b0650ac252ddcb38
priyed <stanleyduye@gmail.com>
2023-01-24 16:20:19 +0100

    refactored db connection to sync with server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("./src/db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./src/routes/tasks");
+const connectDB = require("./src/db/connect");
 
 // middleware
 // without the code below the task data won't be available in the request body
@@ -15,12 +15,15 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-// app.get('/api/v1/tasks') -- get all tasks
-// app.post('/api/v1/tasks') -- create task
-// app.get('/api/v1/tasks/:id) -- get single task
-// app.delete('/api/v1/tasks/:id') -- delete task
-// app.patch('/api/v1/tasks/:id') -- update task
-
 const port = 5001;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
--- src/db/connect.js
+++ src/db/connect.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const connection_string =
   "mongodb+srv://stanley:masterdavid@taskmanagerapp.rflik5x.mongodb.net/task-manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connection_string, {
+const connectDB = () => {
+  return mongoose.connect(connection_string, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("CONNECTED TO THE DB..."))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
